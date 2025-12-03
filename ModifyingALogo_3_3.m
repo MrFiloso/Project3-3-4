e9ab43083dc0a6a56474af70024128386ebd1a36
Antonio <arodriguezdiaz2@my.scccd.edu>
2025-12-03 21:02:26 +0000

    Changes to format of the logo

--- ModifyingALogo_3_3.m
+++ ModifyingALogo_3_3.m
@@ -1,9 +1,8 @@
 
 % Create a script that will change each color in the logo to a new color. 
 % (For example, the red turns blue, the blue turns yellow, and the yellow turns red.
-
-% Step 1: Load the image
-img = imread('Monsterlogo.jpeg.webp');
+% Loading image logo for Moster logo
+img = imread("Monsterlogo.jpg");
 figure;
 imshow(img);
 title('Original Monster Logo');
