1e4f8f553f7e140c859e7f117d367279eb2b8328
Antonio <arodriguezdiaz2@my.scccd.edu>
2025-12-03 21:20:11 +0000

    rework of the script

--- ModifyingALogo_3_3.m
+++ ModifyingALogo_3_3.m
@@ -5,32 +5,16 @@
 img = imread("Screenshot(3).jpg");
 figure;
 title('Original Monster Logo');
+imshow(img);
 % Save the three layers of the array to three dimensional array
-red = logo(:,:,1);
+red= logo(:,:,1);
 green = logo(:,:,2);
 white = logo(:,:,3);
 % Using subplot to show images
 subplot(2,2,1)
 imshow(img);
-% Modify the black color
-black = red<50 & green<50 & blue<50;
-red(black) = 255;
-green(black) = 67;
-blue(black) = 150;
-newlogo(:,:,1) = black;
-newlogo(:,:,2) = green;
-newlogo(:,:,3) = white;
+% Modify the colors
+
 % Display the new age
 subplot(2,2,2)
-imshow(newlogo)
-% White pixels
-white = red>240 & green>240 & blue>240;
-red(white) = 10;
-green(white) = 20;
-blue(white) = 204;
-newlogo(:,:,1) = red;
-newlogo(:,:,2) = green;
-newlogo(:,:,3) = blue;
-% Display the new age
-subplot(2,2,3)
 imshow(newlogo)
