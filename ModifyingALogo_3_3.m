a12741f1439d62571681b4b372d399e4f93181e0
Antonio <arodriguezdiaz2@my.scccd.edu>
2025-12-03 21:37:24 +0000

    Changes to Logo

--- ModifyingALogo_3_3.m
+++ ModifyingALogo_3_3.m
@@ -1,24 +1,31 @@
-
-% Create a script that will change each color in the logo to a new color. 
-% (For example, the red turns blue, the blue turns yellow, and the yellow turns red.
-% Loading image logo for Moster logo
-img = imread("Screenshot(3).jpg");
-figure;
-title('Original Monster Logo');
-imshow(img);
-% Modify the Monster Logo
-img_mod = img;
-% Defining the RGB values
-black= [0, 0, 0];
-green = [124, 252, 0];
-white = [255, 255, 255];
-% Using a for loop to cycle through colors
-
+% Using a script to change the colors on the monster logo
+logo = imread("Screenshot(3).jpg");
+% Save the three layera of the array to three dimensional array
+red = logo(:,:,1);
+green = logo(:,:,2);
+blue = logo(:,:,3);
 % Using subplot to show images
 subplot(2,2,1)
-imshow(img);
-% Modify the colors
-
-% Display the new age
+imshow("Screenshot(3).jpg")
+% Modify the logo
+black = red<50 & green<50 & blue<50;
+red(black) = 255;
+green(black) = 0;
+blue(black) = 0;
+newlogo(:,:,1) = red;
+newlogo(:,:,2) = green;
+newlogo(:,:,3) = blue;
+% Display the new img
 subplot(2,2,2)
+imshow(newlogo)
+% White pixels changed to a different color
+white = red>240 & green>240 & blue>240;
+red(white) = 10;
+green(white) = 20;
+blue(white) = 204;
+newlogo(:,:,1) = red;
+newlogo(:,:,2) = green;
+newlogo(:,:,3) = blue;
+% Display the new img
+subplot(2,2,3)
 imshow(newlogo)
