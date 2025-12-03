1872c2d9e1455c5c5c5a8704e017f7853acc564b
Edwin <0701490@my.scccd.edu>
2025-12-03 20:44:24 +0000

    Create time and velocity arrays and set up the prompt

--- Derivative_Integral_3_2.m
+++ Derivative_Integral_3_2.m
@@ -1,1 +1,11 @@
+%Create a script that will:
+% Determine the position function by integrating the function up to each value of time shown.
+% Create three separate plots: acceleration, velocity, and position.
+% -All graphs should
+%show time values from 0 to 12 seconds
+%include a grid, title and axes labels (include units on your axes labels
 
+%Create time and velocity arrays
+time = 0:1:12;
+vel = [0 1 1.8 2.4 2.6 2.7 2.6 2.2 2 1.9 1.8 1.8 1.8];
+% Determine the acceleration function by using the central difference estimation of the derivative.
