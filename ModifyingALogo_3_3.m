f5758909c120bae71fbfc9920a2770133fce0211
Antonio <arodriguezdiaz2@my.scccd.edu>
2025-12-03 21:49:27 +0000

    Changes to script

--- ModifyingALogo_3_3.m
+++ ModifyingALogo_3_3.m
@@ -6,12 +6,39 @@ green = logo(:,:,2);
 % Using subplot to show images
 subplot(2,2,1)
 imshow("Screenshot(3).jpg")
-% Modify the logo black to yellow
+% Modify the logo to yellow
 black = red<100 & green<100 & blue<100;
 red(black) = 255;
 green(black) = 200;
 newlogo(:,:,1) = red;
 newlogo(:,:,2) = green;
+newlogo(:,:,3) = blue;
 % Display the new img
 subplot(2,2,2)
 imshow(newlogo)
+newlogo(:,:,3) = blue;
+% Display the new age
+subplot(2,2,2)
+imshow(newlogo)
+% White pixel changes
+white = red>240 & green>240 & blue>240;
+red(white) = 10;
+green(white) = 20;
+blue(white) = 204;
+newlogo(:,:,1) = red;
+newlogo(:,:,2) = green;
+newlogo(:,:,3) = blue;
+% Display the new age
+subplot(2,2,3)
+imshow(newlogo)
+% Converting logo
+blue1 = red<200 & green>50 & blue>150;
+red(blue1) = 204;
+green(blue1) = 229;
+blue(blue1) = 255;
+newlogo1(:,:,1) = red;
+newlogo1(:,:,2) = green;
+newlogo1(:,:,3) = blue;
+% Display the new img
+subplot(2,2,4)
+imshow(newlogo1)
