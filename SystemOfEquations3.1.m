12d83b58e7f9635a9a63e97c64ed6c51ee989be5
Edwin Ruelas-Medina <0701490@my.scccd.edu>
2025-12-03 10:33:15 -0800

    Create SystemOfEquations3.1.m

---
+++ SystemOfEquations3.1.m
@@ -0,0 +1,8 @@
+Create a script that will:
+• Ask the user for three sets of (x,y) coordinates that a parabola passes through.
+• Solve a system of equations to determine the coefficients a, b, and c for the equation for the parabola
+𝑦 = 𝑎𝑥2 + 𝑏𝑥 + 𝑐
+• Display the equation for that parabola on the screen
+• Graph the parabola AND the three points that the user entered on the same plot for values of x from -10 to
+10.
+o Include a title and grid for the plot
