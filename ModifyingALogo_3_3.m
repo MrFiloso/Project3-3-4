d31bcf56202b064b9b1b8e996d4d7918501caba9
Antonio <arodriguezdiaz2@my.scccd.edu>
2025-12-03 21:08:35 +0000

    Changes to the script

--- ModifyingALogo_3_3.m
+++ ModifyingALogo_3_3.m
@@ -4,9 +4,19 @@
 % Loading image logo for Moster logo
 img = imread("Screenshot(3).jpg");
 figure;
-imshow(img);
 title('Original Monster Logo');
 % Save the three layers of the array to three dimensional array
-black = logo(:,:,1);
+black1 = logo(:,:,1);
 green = logo(:,:,2);
 white = logo(:,:,3);
+% Using subplot to show images
+subplot(2,2,1)
+imshow(img);
+% Modify the black colors
+black = red<50 & green<50 & blue<50;
+red(black) = 255;
+green(black) = 0;
+blue(black) = 0;
+newlogo(:,:,1) = black;
+newlogo(:,:,2) = green;
+newlogo(:,:,3) = white;
