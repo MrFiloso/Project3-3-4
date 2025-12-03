0469c42f9ce86beddaeb1082f381dc8a38f433e3
Antonio <arodriguezdiaz2@my.scccd.edu>
2025-12-03 21:41:11 +0000

    commiting

--- ModifyingALogo_3_3.m
+++ ModifyingALogo_3_3.m
@@ -3,18 +3,15 @@ logo = imread("Screenshot(3).jpg");
 % Save the three layera of the array to three dimensional array
 red = logo(:,:,1);
 green = logo(:,:,2);
-blue = logo(:,:,3);
 % Using subplot to show images
 subplot(2,2,1)
 imshow("Screenshot(3).jpg")
 % Modify the logo
-black = red<50 & green<50 & blue<50;
+black = red<100 & green<100 & blue<100;
 red(black) = 255;
-green(black) = 0;
-blue(black) = 0;
+green(black) = 200;
 newlogo(:,:,1) = red;
 newlogo(:,:,2) = green;
-newlogo(:,:,3) = blue;
 % Display the new img
 subplot(2,2,2)
 imshow(newlogo)
