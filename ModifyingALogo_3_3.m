344ec9171838ad5edfddea74357e08aed650f3ab
Antonio <arodriguezdiaz2@my.scccd.edu>
2025-12-03 20:52:24 +0000

    Monster logo script commit

--- ModifyingALogo_3_3.m
+++ ModifyingALogo_3_3.m
@@ -1,3 +1,9 @@
 
 % Create a script that will change each color in the logo to a new color. 
-% (For example, the red turns blue, the blue turns yellow, and the yellow turns red.)
+% (For example, the red turns blue, the blue turns yellow, and the yellow turns red.
+
+% Step 1: Load the image
+img = imread('Monsterlogo.jpeg.webp');
+figure;
+imshow(img);
+title('Original Monster Logo');
